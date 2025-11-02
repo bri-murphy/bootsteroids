d6f9819eb3bdded187856a04251e7d0cf713904c
bri-murphy <brian.murphy431@gmail.com>
2025-11-02 11:46:49 -0500

    creating player model and locking game to 60 fps

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import pygame
+from player import *
+from circleshape import *
 from constants import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    dt = 0
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black")
+        player.draw(screen)
         pygame.display.flip()
-
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
