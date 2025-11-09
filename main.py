d15673d07a08e7dabd814b377839b516b4eb4404
bri-murphy <brian.murphy431@gmail.com>
2025-11-08 21:43:03 -0500

    adding logging lesson i missed oops

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from constants import *
 from asteroidfield import *
 
 def main():
+    print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
